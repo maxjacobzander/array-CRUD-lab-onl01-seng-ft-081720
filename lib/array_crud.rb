0591b424496144c1fd8fcdf06f5e1636bc2ea737
Max Zander <maxjacobzander@gmail.com>
2020-08-19 00:07:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  puts create_an_array << "Me"
+  puts create_an_array.push("Me")
 end
 
 def add_element_to_start_of_array(array, element)
