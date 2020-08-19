99eb9e3a45b2f82b922cdc688b3b3fe9b60a8e16
Max Zander <maxjacobzander@gmail.com>
2020-08-19 00:09:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_household [""]
+  my_household  ["Ashley", "Mom", "Dad", "Benjamin"] << "Me"
 end
 
 def add_element_to_start_of_array(array, element)
